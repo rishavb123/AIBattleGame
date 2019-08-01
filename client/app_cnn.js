c750cedeae01128d6dfb5a1aa1d0629b2238c959
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-08-01 10:35:06 -0400

    got nearal network to learn of previous actions

--- client/app_cnn.js
+++ client/app_cnn.js
@@ -49,7 +49,6 @@ class Player extends GameObject {
         this.h = h;
         this.dx = 0;
         this.dy = 0;
-        this.bullets = [];
         this.action = -1;
         this.health = 100;
         this.rgb_color = color;
@@ -59,7 +58,7 @@ class Player extends GameObject {
 
     collide(obj) {
         if (obj instanceof Bullet && obj.sender !== this) {
-            obj.remove();
+            remove(obj);
             this.health -= 10;
         }
     }
@@ -74,35 +73,27 @@ class Player extends GameObject {
     }
 
     shoot() {
-        if(this.bullets.length < 5) {
-            let bullet = new Bullet(this.x + this.w / 4, this.y + this.h / 4, this.w / 2, this.h / 2, (this.dx == 0 && this.dy == 0) ? 3 : 3 * this.dx, 3 * this.dy, this);
-            this.bullets.push(bullet);
-            add(bullet);
-        }
-    }
-
-    getBullet(i) {
-        return this.bullets[i] || Bullet.defaultBullet;
+        add(new Bullet(this.x + this.w / 4, this.y + this.h / 4, this.w / 2, this.h / 2, (this.dx == 0 && this.dy == 0) ? 3 : 3 * this.dx, 3 * this.dy, this));
     }
 
     set_action(action) {
         this.action = action;
         switch (action) {
             case LEFT:
-                this.dx = -1;
-                this.dy = 0;
+                player.dx = -1;
+                player.dy = 0;
                 break;
             case UP:
-                this.dy = -1;
-                this.dx = 0;
+                player.dy = -1;
+                player.dx = 0;
                 break;
             case RIGHT:
-                this.dx = 1;
-                this.dy = 0;
+                player.dx = 1;
+                player.dy = 0;
                 break;
             case DOWN:
-                this.dy = 1;
-                this.dx = 0;
+                player.dy = 1;
+                player.dx = 0;
                 break;
         }
     }
@@ -124,14 +115,8 @@ class Bullet extends GameObject {
 
     update() {
         super.update();
-        if (this.outOfBounds()) {
-            this.remove();
-        }
-    }
-
-    remove() {
-        this.sender.bullets.splice(this.sender.bullets.indexOf(this), 1);
-        remove(this);
+        if (this.outOfBounds())
+            remove(this);
     }
 
 }
@@ -176,25 +161,6 @@ window.addEventListener("keydown", (e) => {
                 shooting = true;
             }
             break;
-
-        case 65:
-            enemy.set_action(LEFT);
-            break;
-        case 87:
-            enemy.set_action(UP);
-            break;
-        case 68:
-            enemy.set_action(RIGHT);
-            break;
-        case 83:
-            enemy.set_action(DOWN);
-            break;
-        case 16:
-            if(!eShooting) {
-                enemy.shoot();
-                eShooting = true;
-            }
-            break;
     }
 });
 
@@ -202,12 +168,6 @@ window.addEventListener("keyup", async (e) => {
     switch(e.keyCode) {
         case 32:
             shooting = false;
-            break;
-        case 16:
-            eShooting = false;
-            break;
-        case 80:
-            agentPlaying = !agentPlaying;
             break;
         case 90:
             const temp = player;
@@ -217,11 +177,8 @@ window.addEventListener("keyup", async (e) => {
             enemy.dy = 0;
             break;
         case 88:
-            running = !running;
-            if(running) animate();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
-            break;
-        case 84:
-            await train(xs, ys);
+            running = false;
+            train();
             break;
     }
 });
@@ -231,11 +188,8 @@ const RIGHT = 1;
 const UP = 2;
 const DOWN = 3;
 
-let xs = [];
-let ys = [];
-
 let running = true;
-let agentPlaying = false;
+let state = [];
 
 const model = create_model();
 
@@ -245,71 +199,91 @@ let player = new Player(0, 0, 10, 10, {
     green: '255',
     blue: '255'
 });
-let eShooting = false;
 let enemy = new Player(10, 10, 10, 10, {
     red: '255',
     green: '0',
     blue: '0'
 });
-
-Bullet.defaultBullet = new Bullet(-1, -1, 0, 0, 0, 0);
-
 let pHealthBar = new HealthBar(2, 2, 30, 5, player);
 let eHealthBar = new HealthBar(68, 2, 30, 5, enemy);
-let gameObjects = [player, pHealthBar]; //, enemy, eHealthBar];
+let gameObjects = [player, pHealthBar];
 let i = 0;
+let xs = []
+let ys = [];
 
 function remove(obj) {
     gameObjects.splice(gameObjects.indexOf(obj), 1);
     if (gameObjects.indexOf(obj) < i)
         i--;
-    delete obj;
 }
 
 function add(obj) {
     gameObjects.push(obj);
 }
 
-function get_state() {
-    let state = [player.x / 100, player.y / 100, player.dx / 100, player.dy / 100, player.health / 100, enemy.x / 100, enemy.y / 100, enemy.health / 100];
-    for(let i = 0; i < 5; i++)
-    {
-        let bullet = player.getBullet(i);
-        state.push(bullet.x / 100, bullet.y / 100, bullet.dx / 100, bullet.dy / 100);
-    }
-    return state;
+async function train() {
+    running = false;
+    // const xDataset = tf.data.array(xs);
+    // const yDataset = tf.data.array(ys);
+    // const xyDataset = tf.data.zip({xs: xDataset, ys: yDataset})
+    //     .batch(4)
+    //     .shuffle(4);
+    // await model.fitDataset(xyDataset, {
+    //     epochs: 10,
+    //     callbacks: {onEpochEnd: (epoch, logs) => console.log(logs.loss)}
+    // });
+    await model.save('downloads://model-1');
 }
 
-function get_opposite_state() {
-    let state = [enemy.x / 100, enemy.y / 100, enemy.dx / 100, enemy.dy / 100, enemy.health / 100, player.x / 100, player.y / 100, player.health / 100];
-    for(let i = 0; i < 5; i++)
-    {
-        let bullet = enemy.getBullet(i);
-        state.push(bullet.x / 100, bullet.y / 100, bullet.dx / 100, bullet.dy / 100);
-    }
-    return state;
+async function get_pixels() {
+    const imageData = ctx.getImageData(0, 0, c.width, c.height);
+    const imageTensor = tf.image.resizeNearestNeighbor(tf.browser.fromPixels(imageData), [40, 40]);
+    let = imageMatrix = await imageTensor.array();
+    imageTensor.dispose();
+    imageMatrix = imageMatrix.map(row => row.map(p => (.2126 * p[0] + .7152 * p[1] + .0722 * p[2]) / 255));
+    imageMatrix = tf.tensor(imageMatrix);
+    return imageMatrix;
+}
+
+async function update_state() {
+    state.push(await get_pixels());
+    if(state.length > 4)
+        state.shift().dispose();
+}
+
+function get_state() {
+    return tf.stack(state, 2);
 }
 
 function create_model() {
     const model = tf.sequential();
   
-    const INPUT_NODES = 28;
-    const HIDDEN_LAYERS = [30, 30, 30];
-    const NUM_OUTPUT_CLASSES = 5;
+    const IMAGE_WIDTH = 40;
+    const IMAGE_HEIGHT = 40;
+    const IMAGE_CHANNELS = 4; 
 
-    model.add(tf.layers.dense({
-        inputDim: INPUT_NODES,
-        units: INPUT_NODES,
-        kernelInitializer: 'varianceScaling',
-        activation: 'tanh'
+    const NUM_OUTPUT_CLASSES = 4;
+    
+    model.add(tf.layers.conv2d({
+        inputShape: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
+        kernelSize: 7,
+        filters: 8,
+        strides: 3,
+        activation: 'relu',
+        kernelInitializer: 'varianceScaling'
     }));
+    model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
 
-    for(let NUM_OF_HIDDEN_NODES of HIDDEN_LAYERS)
-        model.add(tf.layers.dense({
-            units: NUM_OF_HIDDEN_NODES,
-            kernelInitializer: 'varianceScaling',
-            activation: 'relu'
-        }));
+    model.add(tf.layers.conv2d({
+        kernelSize: 7,
+        filters: 16,
+        strides: 3,
+        activation: 'relu',
+        kernelInitializer: 'varianceScaling'
+    }));
+    model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
+
+    model.add(tf.layers.flatten());
 
     model.add(tf.layers.dense({
         units: NUM_OUTPUT_CLASSES,
@@ -323,30 +297,27 @@ function create_model() {
         loss: 'categoricalCrossentropy',
         metrics: ['accuracy']
     });
+    // model.save('downloads://random-model');
     return model;
 }
 
-async function train(xs, ys) {
-    console.log("Training . . .");
-    const xDataset = tf.data.array(xs);
-    const yDataset = tf.data.array(ys);
-    const xyDataset = tf.data.zip({xs: xDataset, ys: yDataset})
-        .batch(4)
-        .shuffle(4);
-    await model.fitDataset(xyDataset, {
-        epochs: 10,
-        callbacks: {onEpochEnd: (epoch, logs) => console.log("Epoch " + epoch + ": " + logs.loss)}
-    });
-    console.log("Done Training!");
+function send_request() {
+    const Http = new XMLHttpRequest();
+    const state = get_state();
+    const url='http://localhost:4000?' + state[0] + ";" + state[1];
+    console.log("sending thing to: " + url)
+    Http.open("GET", url);
+    Http.send();
+
+    Http.onreadystatechange = (e) => {
+        console.log(Http.responseText)
+    }
 }
 
-function animate() {
+async function animate() {
     if(running)
     requestAnimationFrame(animate);
     ctx.clearRect(0, 0, c.width, c.height);
-    if(agentPlaying) {
-        player.set_action(model.predict(tf.tensor([get_opposite_state()])).argMax(1).dataSync()[0]);
-    }
     for (i = 0; i < gameObjects.length; i++) {
         const obj = gameObjects[i];
         obj.update();
@@ -358,13 +329,16 @@ function animate() {
             }
         }
     }
-    if(player.action >= 0) {
-        xs.push(get_opposite_state());
-        let y = [0, 0, 0, 0, 0];
-        y[player.action] = 1;
-        ys.push(y);
-    }
-    
+    // await update_state();
+    // if(state.length == 4) {
+    //     let arr = [0, 0, 0, 0];
+    //     arr[player.action] = 1;
+    //     const s = get_state().reshape([1, 40, 40, 4]);
+    //     // console.log(s.shape);
+    //     // console.log(model);
+    //     await model.fit(s, tf.tensor([arr]));
+    //     console.log(model.predict(s));
+    // }
 }
 
 animate();
